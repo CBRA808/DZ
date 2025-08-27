6d662cad3b8bdcabde0613427e458cda0a9e73e2
CBRA808 <naness2278@gmail.com>
2025-08-27 16:13:07 +0100

    Update NINO.py

--- NINO.py
+++ NINO.py
@@ -1,10 +1,4 @@
-import importlib
+import NINO
 
-# استدعاء الملف المشفر NINO.cpython-312.so
-NINO = importlib.import_module("NINO")
-
-# شغّل الدالة الرئيسية (بدّل 'main' بالاسم الصحيح داخل الملف المشفر)
-try:
-    NINO.main()
-except AttributeError:
-    print("[!] لم أجد الدالة 'main' داخل الملف المشفر NINO")
+# استدعاء الدالة لي كاينة فالسكريبت الأصلي
+NINO.mrpoco()
